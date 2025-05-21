897405317c6b3d3c7be84d53373ba791ce5d584f
venkatsait33 <129149218+venkatsait33@users.noreply.github.com>
2025-05-21 17:49:28 +0530

    footer update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -21,30 +21,30 @@ const HomePage = () => {
                         <button className='btn hover:bg-[#1F8268] btn-outline border border-[#1F8268]'>View all</button>
                     </div>
                 </div>
-                <div className='p-8 bg-white '>
+                <div className='bg-white max-sm:p-4 md:p-8 '>
 
-                    <div className='bg-[#F9F4FF] text-[#662194] flex items-center justify-center border rounded-l-2xl w-[80%] mx-auto md:h-[350px] gap-12  '>
+                    <div className='bg-[#F9F4FF] text-[#662194] flex items-center justify-center border rounded-l-2xl md:w-[80%] mx-auto md:h-[350px] md:gap-12 p-2 '>
                         <div>
-                            <h1 className='text-3xl font-bold'>Download Apna app!</h1>
-                            <div className='flex gap-2'>
+                            <h1 className='font-bold md:text-3xl'>Download Apna app!</h1>
+                            <div className='flex gap-2 max-sm:flex-col max-sm:text-sm'>
                                 <p>Unlimited job applications | </p>
                                 <p>HRs contact you directly | </p>
                                 <p>Track your Applications</p>
                             </div>
                         </div>
-                        <div>
+                        <div className=''>
                             <img src="https://storage.googleapis.com/mumbai_apnatime_prod/apna-home/apna-app.png"
-                                loading='lazy' className='h-[200px]' alt="" />
+                                loading='lazy' className='md:h-[200px] max-sm:h-[100px]' alt="" />
                         </div>
                         <div>
                             <div>
-                                <p className='flex items-center gap-1 text-2xl font-semibold'> <span className=' text-[#FFD166]'>
+                                <p className='flex items-center gap-1 font-semibold md:text-2xl'> <span className=' text-[#FFD166]'>
                                     <FaStar />
                                 </span>4.4</p>
                                 <p>5L reviews</p>
                             </div>
                             <div>
-                                <p className='flex items-center gap-1 text-2xl font-semibold'><span>
+                                <p className='flex items-center gap-1 font-semibold md:text-2xl'><span>
                                     <MdFileDownload />
                                 </span>1 cr+</p>
                                 <p>App downloads</p>
